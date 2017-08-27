435e0f51ec28e9334e624e2cab293eb634ef4921
Javier <javier.ortega.ubago@gmail.com>
2017-08-28 00:37:04 +0200

    Primera version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { RobotComponent } from './robot/robot.component';
+import { InicioComponent } from './inicio/inicio.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent },
+  {
+    path: 'robot',
+    component: RobotComponent },
+  {
+    path: '',
+    component: InicioComponent },
+  {
+    path: 'login',
+    component: LoginComponent }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    RobotComponent,
+    InicioComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes
+    )
+    ,
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot(),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
